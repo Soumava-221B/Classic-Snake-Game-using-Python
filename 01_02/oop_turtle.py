fa8e2f92db64e21e4083980492960e580c025fde
Soumava Das <soumavadas06@gmail.com>
2025-03-06 20:19:16 +0530

    initial commit

---
+++ 01_02/oop_turtle.py
@@ -0,0 +1,12 @@
+# Import Turtle Graphics module
+import turtle
+
+# Create a turtle to do your bidding
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("red")
+
+# Your turtle awaits your command
+my_turtle.forward(100)
+
+turtle.done()
